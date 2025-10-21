a280f9209f07fbc8caed3a68d1c3eb432fc794b1
Alex Kiburu <kiburu.alex@eclectics.io>
2025-10-21 12:38:53 +0300

    initial commit

---
+++ src/main/java/io/firogence/mobile_banking_channel_request_processor/MobileBankingChannelRequestProcessorApplication.java
@@ -0,0 +1,13 @@
+package io.firogence.mobile_banking_channel_request_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileBankingChannelRequestProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileBankingChannelRequestProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/firogence/mobile_banking_channel_request_processor/MobileBankingChannelRequestProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.firogence.mobile_banking_channel_request_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileBankingChannelRequestProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
